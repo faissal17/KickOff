a298b95120032a451850f9322d30fa98acd16d37
faissal17 <faissalaoukacha@gmail.com>
2024-02-19 18:31:42 +0100

    start working on home screen

---
+++ src/components/screen/HomeScreen.js
@@ -0,0 +1,16 @@
+import { View, Text, ImageBackground } from 'react-native';
+import React from 'react';
+import { HomeScreenStyle } from '../../styles/globalStyle';
+import Logo from '../../assets/images/Logo.png';
+
+const HomeScreen = () => {
+  return (
+    <ImageBackground source={Logo} style={HomeScreenStyle.background}>
+      <View style={HomeScreenStyle.root}>
+        <Text>HomeScreen</Text> 
+      </View>
+    </ImageBackground>
+  );
+};
+
+export default HomeScreen;
---
+++ src/styles/globalStyle.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+export const HomeScreenStyle = StyleSheet.create({
+  root: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+});
